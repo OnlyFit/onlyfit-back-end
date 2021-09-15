0f9847a3c6bffe1c8d88ab2935eb83140fdf7269
DiegoMurcia2022 <diegomurciac2220@gmail.com>
2021-09-14 21:43:54 -0500

    Project start.

---
+++ src/main/java/ieti/project/onlyfit/OnlyfitApplication.java
@@ -0,0 +1,13 @@
+package ieti.project.onlyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ieti/project/onlyfit/OnlyfitApplicationTests.java
@@ -0,0 +1,13 @@
+package ieti.project.onlyfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
